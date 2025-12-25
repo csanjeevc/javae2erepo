25e4d41ac2504696d732ab9f4201f377ae698dc4
Sanjeev Chouksey <csanjeev@gmail.com>
2025-12-25 16:31:19 +0530

    Initial commit: Maven microservice with Docker

--- src/main/java/com/example/java_e2e/JavaE2eApplication.java
+++ src/main/java/com/example/java_e2e/JavaE2eApplication.java
@@ -15,6 +15,6 @@ public class JavaE2eApplication
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World")
             String name) {
-        return String.format("Hello %s!", name);
+        return String.format("Hello-1 %s!", name);
     }
 }
